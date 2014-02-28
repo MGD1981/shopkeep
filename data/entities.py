074c42d7f393ab1125ad81da789782957fe91bfc
Matthew Davis <mgd1981@gmail.com>
2014-02-28 20:25:10 +0000

    Added player to global entities

--- data/entities.py
+++ data/entities.py
@@ -2,7 +2,9 @@ import pickle
 
 
 def initialize():
-    global weapons, heroes, adventure_sites, resource_sites
+    global player, weapons, heroes, adventure_sites, resource_sites
+    import player_data
+    player = player_data.get_new_player()
     weapons = {}
     heroes = {}
     adventure_sites = {}
@@ -11,21 +13,23 @@ def initialize():
 
 
 def load(save_name):
-    global weapons, heroes, adventure_sites, resource_sites
+    global player, weapons, heroes, adventure_sites, resource_sites
     save_file = open('saves/%s' % save_name)
     dict_list = pickle.load(save_file)
-    weapons = dict_list[0]
-    heroes = dict_list[1]
-    adventure_sites = dict_list[2]
-    resource_sites = dict_list[3]
+    player = dict_list[0]
+    weapons = dict_list[1]
+    heroes = dict_list[2]
+    adventure_sites = dict_list[3]
+    resource_sites = dict_list[4]
     save_file.close()
     return
 
 
 def save(save_name):
-    global weapons, heroes, adventure_sites, resource_sites
+    global player, weapons, heroes, adventure_sites, resource_sites
     save_file = open('saves/%s' % save_name)
-    dict_list = [weapons, 
+    dict_list = [player,
+                 weapons, 
                  heroes, 
                  adventure_sites, 
                  resource_sites]
