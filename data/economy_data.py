0f96cd46dd517d245990b0b424c8dd717e3ef859
Matthew Davis <mgd1981@gmail.com>
2014-03-04 16:30:42 +0000

    economy_data.py edited online with Bitbucket

--- data/economy_data.py
+++ data/economy_data.py
@@ -6,10 +6,14 @@ class Economy():
 	"""Economy class object."""
 	
 	def __init__():
+		weapon_value_table = {}
+		material_value_table = {}
+		
+		#A specific weapon has its value stored by adding its weapon_id key
 		for weapon_type in weapon_data.Weapon.weapon_type_dct:
-			exec("self.%s = None" % weapon_type) #initializes each weapon_type price as None
+			weapon_value_table[weapon_type] = None #initializes each weapon_type price as None
 		for material_class in weapon_data.Material.material_class_dct:
 			for material_type in material_class['type']:
-				exec("self.%s = None" % material_type) #initializes each material_type price as None
+				material_value_table[material_type] = None #initializes each material_type price as None
 
 			
