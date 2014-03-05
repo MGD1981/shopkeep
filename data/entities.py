d0594ac4e774de0a0527e09001f12cc2a67d12cd
Matthew Davis <mgd1981@gmail.com>
2014-03-05 19:05:31 +0000

    entities.py edited online with Bitbucket

--- data/entities.py
+++ data/entities.py
@@ -1,12 +1,21 @@
 import pickle
 
+player = {}
+weapons = {}
+components = {}
+heroes = {}
+adventure_sites = {}
+resource_sites = {}
+world = {}
+town = {}
 
 
 def initialize():
-    global player, weapons, heroes, adventure_sites, resource_sites, world, town
+    global player, weapons, components, heroes, adventure_sites, resource_sites, world, town
     import player_data
     player = player_data.get_new_player()
     weapons = {}
+    components = {}
     heroes = {}
     adventure_sites = {}
     resource_sites = {}
@@ -16,25 +25,27 @@ def initialize():
 
 
 def load(save_name):
-    global player, weapons, heroes, adventure_sites, resource_sites, world, town
+    global player, weapons, components, heroes, adventure_sites, resource_sites, world, town
     save_file = open('saves/%s' % save_name)
     dict_list = pickle.load(save_file)
     player = dict_list[0]
     weapons = dict_list[1]
-    heroes = dict_list[2]
-    adventure_sites = dict_list[3]
-    resource_sites = dict_list[4]
-    world = dict_list[5]
-    town = dict_list[6]
+    components = dict_list[2]
+    heroes = dict_list[3]
+    adventure_sites = dict_list[4]
+    resource_sites = dict_list[5]
+    world = dict_list[6]
+    town = dict_list[7]
     save_file.close()
     return
 
 
 def save(save_name):
-    global player, weapons, heroes, adventure_sites, resource_sites, world, town
+    global player, weapons, components, heroes, adventure_sites, resource_sites, world, town
     save_file = open('saves/%s' % save_name)
     dict_list = [player,
-                 weapons, 
+                 weapons,
+                 components,
                  heroes, 
                  adventure_sites, 
                  resource_sites,
