0a4f72a6292d8920b91eed453ea79c350a312482
Matthew Davis <mgd1981@gmail.com>
2014-03-05 17:42:02 +0000

    Changed references to dicts in weapon_data to reference_data

--- data/economy_data.py
+++ data/economy_data.py
@@ -1,5 +1,5 @@
 import entities
-import weapon_data
+import reference_data as ref
 
 
 class Economy():
@@ -10,9 +10,9 @@ class Economy():
 		material_value_table = {}
 		
 		#A specific weapon has its value stored by adding its weapon_id key
-		for weapon_type in weapon_data.Weapon.weapon_type_dct:
+		for weapon_type in ref.weapon_type_dct:
 			weapon_value_table[weapon_type] = None #initializes each weapon_type price as None
-		for material_class in weapon_data.Material.material_class_dct:
+		for material_class in ref.material_class_dct:
 			for material_type in material_class['type']:
 				material_value_table[material_type] = None #initializes each material_type price as None
 
