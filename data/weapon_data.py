5258f1ea9b7579331336b14b482b05cfb86a4022
gremdavel <MGD1981@gmail.com>
2014-03-04 22:07:32 -0500

    In middle of attempt to add joint generation logic.

--- data/weapon_data.py
+++ data/weapon_data.py
@@ -1,5 +1,5 @@
 import entities
-from random import choice
+from random import choice, randint
 
 
 class Weapon():
@@ -8,37 +8,101 @@ class Weapon():
     weapon_type_dct = {
         'dirk': {
             'class': 'dagger',
-            'components': ['hilt', 'blade', 'scabbard'],
+            'components': {
+                'hilt': {
+                    'joints': ['blade']
+                },
+                'blade': {
+                    'joints': ['hilt']
+                },
+                'scabbard': {
+                    'joints': []
+                },
+            },
             'hands to wield': 1
         },
         'shortsword': {
             'class': 'sword',
-            'components': ['hilt', 'blade', 'scabbard'],
+            'components': {
+                'hilt': {
+                    'joints': ['blade']
+                },
+                'blade': {
+                    'joints': ['hilt']
+                },
+                'scabbard': {
+                    'joints': []
+                }
+            },
             'hands to wield': 1
         },
         'longsword': {
             'class': 'sword',
-            'components': ['hilt', 'blade', 'scabbard'],
+            'components': {
+                'hilt': {
+                    'joints': ['blade']
+                },
+                'blade': {
+                    'joints': ['hilt']
+                },
+                'scabbard': {
+                    'joints': []
+                }
+            },
             'hands to wield': 2
         },
         'flail': {
             'class': 'blunt',
-            'components': ['handle', 'chain', 'ball'],
+            'components': {
+                'handle': {
+                    'joints': ['chain']
+                },
+                'chain': {
+                    'joints': ['handle', 'ball']
+                },
+                'ball': {
+                    'joints': ['chain']
+                }
+            },
             'hands to wield': 1
         },
         'double flail': {
             'class': 'blunt',
-            'components': ['handle', 'chain', 'ball', 'chain', 'ball'],
+            'components': {
+                'handle': {
+                    'joints': ['chain', 'chain']
+                },
+                'chain': {
+                    'joints': ['handle', 'ball']
+                },
+                'ball': {
+                    'joints': ['chain']
+                },
+                'chain': {
+                    'joints': ['handle', 'ball']
+                },
+                'ball': {
+                    'joints': ['chain']
+                }
+            },
             'hands to wield': 1
         },
         'battle axe': {
             'class': 'cleave',
-            'components': ['haft', 'blade'],
+            'components': {
+                'haft': {
+                    'joints': ['blade']
+                },
+                'blade': {
+                    'joints': ['haft'],
+                }
+            },
             'hands to wield': 2
         }
     }
 
-    #TODO: Once a weapon of every class exists, weapon_class_dct may be generated with an init function on weapon_type_dct.
+    #TODO: Once a weapon of every class exists, weapon_class_dct may be 
+    #generated with an init function on weapon_type_dct.
     #Hardcoded data just for reference right now.
     weapon_class_dct = {
         'dagger': [
@@ -75,7 +139,7 @@ class Weapon():
         self.weapon_class = None
         self.owners = []
         self.kills = []
-        self.components = [] #Each component has Joint objects connecting Components
+        self.components = [] #Each component has Joint objects connecting it.
         
     def print_stats(self):
         """Prints the weapon's information in the console."""
@@ -88,6 +152,14 @@ class Weapon():
             for material in component.materials:
                 print "        %r (%r)" % (
                     material.material_type, material.material_class)
+#            for joint in component.joints:
+#                joined = None
+#                for c in self.components:
+#                    if joint in c.joints and c != component:
+#                        joined = c
+#                        break
+#                print "        with a %r joint connecting to the %r." (
+#                    joint.material.material_type, joined.component_type)
         print "\n"
 
     def generate(self, arg='random'):
@@ -99,6 +171,25 @@ class Weapon():
                     self.weapon_type]['components']:
                 self.components.append(
                         Component().generate(component))
+            components_to_connect = Weapon.weapon_type_dct[
+                                    self.weapon_type]['components']
+            n = 0
+            for c in components_to_connect:
+                n += len(components_to_connect[c]['joints'])
+            number_of_joints_to_connect = n/2
+            components_connected = []
+            joints_to_connect = []
+            for x in xrange(number_of_joints_to_connect):
+                joints_to_connect.append(Joint().generate())
+#            for component_to_connect in components_to_connect:
+#                for component in self.components:
+#                    if component.component_type == component_to_connect:
+                        #connect if not already connected
+                        #test if connected:
+                        #TODO: Build some helper functions
+
+
+            
         else:
             return NotImplementedError(arg)
         return self
@@ -133,6 +224,7 @@ class Component():
     def __init__(self):
         self.component_type = None
         self.materials = []
+        self.joints = []
 
     def generate(self, component_type, arg='random'):
         if arg == 'random':
@@ -149,11 +241,21 @@ class Component():
 class Joint():
 
     def __init__(self):
-        self.connected_components = [] #list of component objects
-        self.material_type = None #material of which joint is made
+        self.material = None #material of which joint is made
         self.material_integrity = None #i.e. 100=new, 0=broken
         self.joint_quality = None #higher quality = slower rate of deterioration
         self.joint_integrity = None #i.e. 100-new, 0=broken
+
+    def generate(self, arg='random'):
+        if arg == 'random':
+            self.material = Material().generate('metal')
+            self.material_integrity = randint(1,100)
+            self.joint_quality = randint(1,100)
+            self.joint_integrity = randint(1,100)
+        else:
+            return NotImplementedError(arg)
+        return self
+        
         
 
 class Material():
