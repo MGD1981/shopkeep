cb8d0d2828daeba0c59e7206e461c8b6735e8c85
Matthew Davis <mgd1981@gmail.com>
2014-03-05 14:47:00 +0000

    Moved reference dicts to reference_data library.

--- data/weapon_data.py
+++ data/weapon_data.py
@@ -1,137 +1,10 @@
 import entities
+import reference_data as ref
 from random import choice, randint
 
 
 class Weapon():
-
-    #TODO: Move dct reference objects to dct class
-    weapon_type_dct = {
-        'dirk': {
-            'class': 'dagger',
-            'components': {
-                'hilt': {
-                    'joints': ['blade']
-                },
-                'blade': {
-                    'joints': ['hilt']
-                },
-                'scabbard': {
-                    'joints': []
-                },
-            },
-            'hands to wield': 1
-        },
-        'shortsword': {
-            'class': 'sword',
-            'components': {
-                'hilt': {
-                    'joints': ['blade']
-                },
-                'blade': {
-                    'joints': ['hilt']
-                },
-                'scabbard': {
-                    'joints': []
-                }
-            },
-            'hands to wield': 1
-        },
-        'longsword': {
-            'class': 'sword',
-            'components': {
-                'hilt': {
-                    'joints': ['blade']
-                },
-                'blade': {
-                    'joints': ['hilt']
-                },
-                'scabbard': {
-                    'joints': []
-                }
-            },
-            'hands to wield': 2
-        },
-        'flail': {
-            'class': 'blunt',
-            'components': {
-                'handle': {
-                    'joints': ['chain']
-                },
-                'chain': {
-                    'joints': ['handle', 'ball']
-                },
-                'ball': {
-                    'joints': ['chain']
-                }
-            },
-            'hands to wield': 1
-        },
-        'double flail': {
-            'class': 'blunt',
-            'components': {
-                'handle': {
-                    'joints': ['chain', 'chain']
-                },
-                'chain': {
-                    'joints': ['handle', 'ball']
-                },
-                'ball': {
-                    'joints': ['chain']
-                },
-                'chain': {
-                    'joints': ['handle', 'ball']
-                },
-                'ball': {
-                    'joints': ['chain']
-                }
-            },
-            'hands to wield': 1
-        },
-        'battle axe': {
-            'class': 'cleave',
-            'components': {
-                'haft': {
-                    'joints': ['blade']
-                },
-                'blade': {
-                    'joints': ['haft'],
-                }
-            },
-            'hands to wield': 2
-        }
-    }
-
-    #TODO: Once a weapon of every class exists, weapon_class_dct may be 
-    #generated with an init function on weapon_type_dct.
-    #Hardcoded data just for reference right now.
-    weapon_class_dct = {
-        'dagger': [
-            'dirk'
-        ],
-        'sword': [
-            'shortsword',
-            'longsword'
-        ],
-        'blunt': [
-            'flail',
-            'double flail'
-        ],
-        'cleave': [
-            'battle axe'
-        ],
-        'polearm': [
-        
-        ],
-        'bow': [
-        
-        ],
-        'ammunition': [
-        
-        ],
-        'projectile': [
-        
-        ]
-    }
+    """Weapon class object with unique weapon_id.  Contains Component objects."""
 
     def __init__(self):
         self.weapon_id = None #TODO: get unique based on entities.weapons
@@ -164,14 +37,14 @@ class Weapon():
 
     def generate(self, arg='random'):
         if arg == 'random':
-            self.weapon_type = choice(Weapon.weapon_type_dct.keys())
-            self.weapon_class = Weapon.weapon_type_dct[
+            self.weapon_type = choice(ref.weapon_type_dct.keys())
+            self.weapon_class = ref.weapon_type_dct[
                                         self.weapon_type]['class']
-            for component in Weapon.weapon_type_dct[
+            for component in ref.weapon_type_dct[
                     self.weapon_type]['components']:
                 self.components.append(
                         Component().generate(component))
-            components_to_connect = Weapon.weapon_type_dct[
+            components_to_connect = ref.weapon_type_dct[
                                     self.weapon_type]['components']
             n = 0
             for c in components_to_connect:
@@ -196,30 +69,7 @@ class Weapon():
 
 
 class Component():
-
-    component_type_dct = {
-        'hilt': {
-            'possible materials': ['wood', 'stone', 'metal']
-        },
-        'blade': {
-            'possible materials': ['metal']
-        },
-        'scabbard': {
-            'possible materials': ['leather', 'wood', 'metal']
-        },
-        'haft': {
-            'possible materials': ['wood']
-        },
-        'handle': {
-            'possible materials': ['wood']
-        },
-        'chain': {
-            'possible materials': ['metal']
-        },
-        'ball': {
-            'possible materials': ['metal']
-        }
-    }
+    """Component object class.  Contains Material objects and Joint objects."""
 
     def __init__(self):
         self.component_type = None
@@ -231,7 +81,7 @@ class Component():
             self.component_type = component_type
             self.materials.append(
                     Material().generate(choice(
-                    Component.component_type_dct[
+                    ref.component_type_dct[
                     component_type]['possible materials'])))
         else:
             return NotImplementedError(arg)
@@ -239,6 +89,7 @@ class Component():
 
 
 class Joint():
+    """Joint object class.  May exist as attribute of multiple Component objects."""
 
     def __init__(self):
         self.material = None #material of which joint is made
@@ -259,106 +110,7 @@ class Joint():
         
 
 class Material():
-
-    material_class_dct = {
-        'wood': {
-            'type': {
-                'bodark': {
-                    'strength': None,
-                    'flexibility': None,
-                    'density': None
-                },
-                'epay': {
-                    'strength': None,
-                    'flexibility': None,
-                    'density': None
-                },
-                'lemonwood': {
-                    'strength': None,
-                    'flexibility': None,
-                    'density': None
-                },
-                'pignut': {
-                    'strength': None,
-                    'flexibility': None,
-                    'density': None
-                },
-                'hickory': {
-                    'strength': None,
-                    'flexibility': None,
-                    'density': None
-                },
-                'oak': {
-                    'strength': None,
-                    'flexibility': None,
-                    'density': None
-                },
-                'maple': {
-                    'strength': None,
-                    'flexibility': None,
-                    'density': None
-                },
-                'yew': {
-                    'strength': None,
-                    'flexibility': None,
-                    'density': None
-                },
-                'elm': {
-                    'strength': None,
-                    'flexibility': None,
-                    'density': None
-                },
-                'ash': {
-                    'strength': None,
-                    'flexibility': None,
-                    'density': None
-                }
-            } 
-        },
-        'fiber': {
-            'type': {
-                'linen': {},
-                'hemp': {},
-                'sinew': {},
-                'silk': {},
-                'rawhide': {}
-            }
-        },
-        'stone': {
-            'type': {
-                'chert': {},
-                'flint': {},
-                'granite': {},
-                'marble': {}
-            }
-        },
-        'leather': {
-            'type': {
-                'cow leather': {},
-                'buffalo leather': {},
-                'goat hide': {},
-                'calfskin': {},
-                'sheepskin': {},
-                'deerskin': {},
-                'elkskin': {},
-                'horse leather': {},
-                'sharkskin': {},
-                'dragonhide': {}
-            }
-        },
-        'metal': {
-            'type': {
-                'copper': {},
-                'bronze': {},
-                'silver': {},
-                'gold': {},
-                'iron': {},
-                'steel': {},
-                'silvered steel': {},
-                'adamantine steel': {}
-            }
-        }
-    }
+    """Material object class."""
 
     def __init__(self):
         self.material_class = None
@@ -369,7 +121,7 @@ class Material():
         self.material_class = material_class
         if arg == 'random':
             self.material_type = choice(
-                            Material.material_class_dct[
+                            ref.material_class_dct[
                                 material_class]['type'].keys())
         else:
             return NotImplementedError(arg)
