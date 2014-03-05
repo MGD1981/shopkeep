eb9c2275155164860e3c35bc0ecc7bdbe0de17eb
Matthew Davis <mgd1981@gmail.com>
2014-03-05 21:49:46 +0000

    reference_data.py edited online with Bitbucket

--- data/reference_data.py
+++ data/reference_data.py
@@ -94,7 +94,7 @@ component_type_dct = {
     'chain': {
         'class': 'flagellum',
         'possible materials': ['metal'],
-        'joints': (('single', 'handle'), ('single', 'finger'))
+        'joints': (('single', 'handle'), ('optional', 'finger'))
     },
     'ball': {
         'class': 'finger',
