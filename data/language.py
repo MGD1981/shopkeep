b3eef5822374d414eb5ae588dc95d289e0e49ddf
Matthew Davis <mgd1981@gmail.com>
2014-03-07 19:07:55 +0000

    language.py edited online with Bitbucket

--- data/language.py
+++ data/language.py
@@ -1,42 +1,26 @@
 import letter_dct from reference.data
 from random import choice, randint
 
-def create_monster_name():
+def create_name(name_type='human'):
 	"""Generates a random name based on rules."""
 	
-	name_chars = []
-	start_with = choice(['consonant', 'vowel'])
-	if start_with == 'vowel':
-		name_chars += choice(letter_dct['m_vowel start'])
-		name_chars += choice(letter_dct['m_consonant end'])
-	if start_with == 'consonant':
-		if randint(0, 1) == 0:
-			name_chars += choice(letter_dct['m_consonant start'])
-			name_chars += choice(letter_dct['m_vowel mid'])
-		else:
-			name_chars += choice(letter_dct['m_consonant start'])
-			name_chars += choice(letter_dct['m_vowel mid'])
-			name_chars += choice(letter_dct['m_consonant end'])
-	name = ''.join(name_chars)
-	return name.capitalize()
-	
-	
-def create_human_name():
-	"""Generates a name with more stringent rules."""
-	
-	name_chars = []
-	start_with = choice(['consonant', 'vowel'])
-	if start_with == 'vowel':
-		name_chars += choice(letter_dct['h_vowel start'])
-		name_chars += choice(letter_dct['h_consonant end'])
-	if start_with == 'consonant':
-		if randint(0, 1) == 0:
-			name_chars += choice(letter_dct['h_consonant start'])
-			name_chars += choice(letter_dct['h_vowel mid'])
-		else:
-			name_chars += choice(letter_dct['h_consonant start'])
-			name_chars += choice(letter_dct['h_vowel mid'])
-			name_chars += choice(letter_dct['h_consonant end'])
-	name = ''.join(name_chars)
-	return name.capitalize()
-		
+    if name_type == 'human':
+        syllables = randint(1, 2)
+    if name_type == 'monster':
+        syllables = 1
+    name_chars = []
+    for x in xrange(syllables):
+    	start_with = choice(['consonant', 'vowel'])
+    	if start_with == 'vowel':
+    		name_chars += choice(letter_dct[name_type]['vowel start'])
+    		name_chars += choice(letter_dct[name_type]['consonant end'])
+    	if start_with == 'consonant':
+    		if randint(0, 1) == 0:
+    			name_chars += choice(letter_dct[name_type]['consonant start'])
+    			name_chars += choice(letter_dct[name_type]['vowel mid'])
+    		else:
+    			name_chars += choice(letter_dct[name_type]['consonant start'])
+    			name_chars += choice(letter_dct[name_type]['vowel mid'])
+    			name_chars += choice(letter_dct[name_type]['consonant end'])
+
+	return ''.join(name_chars).capitalize()
