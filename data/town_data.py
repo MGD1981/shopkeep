65008387b4bcfa10f0dbb24c3e769418535e41f2
Matthew Davis <mgd1981@gmail.com>
2014-03-10 20:15:10 +0000

    town_data.py created online with Bitbucket

---
+++ data/town_data.py
@@ -0,0 +1,16 @@
+import reference_data as ref
+
+def get_new_town():
+    town = {
+        'resource': {}
+    }
+	for material_class in ref.material_class_dct.keys():
+		town['resource'][material_class] = {}
+		for material_type in ref.material_class_dct[material_class]:
+			town['resource'][material_class][material_type] = {
+				'intrinsic value': 0,
+				'harvestable': 0,
+				'available': 0
+			}
+
+    return world
