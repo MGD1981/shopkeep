340af66551aec6ac38fec37a627da2e67c45fbe9
Matthew Davis <mgd1981@gmail.com>
2014-03-10 20:16:22 +0000

    town_data.py edited online with Bitbucket

--- data/town_data.py
+++ data/town_data.py
@@ -4,13 +4,13 @@ def get_new_town():
     town = {
         'resource': {}
     }
-	for material_class in ref.material_class_dct.keys():
-		town['resource'][material_class] = {}
-		for material_type in ref.material_class_dct[material_class]:
-			town['resource'][material_class][material_type] = {
-				'intrinsic value': 0,
-				'harvestable': 0,
-				'available': 0
-			}
+    for material_class in ref.material_class_dct.keys():
+        town['resource'][material_class] = {}
+        for material_type in ref.material_class_dct[material_class]:
+            town['resource'][material_class][material_type] = {
+                'intrinsic value': 0,
+                'harvestable': 0,
+                'available': 0
+            }
 
     return world
