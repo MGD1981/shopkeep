575d7c4481bda0c3eb573ab04b606d4b9457e75b
Matthew Davis <mgd1981@gmail.com>
2014-03-12 21:38:32 +0000

    Added occupations to town object.

--- data/town_data.py
+++ data/town_data.py
@@ -2,13 +2,24 @@ import reference_data as ref
 
 def get_new_town():
     town = {
-        'resource': {}
+        'resource': {},
+        'occupation': {
+            'adventurer': 0,
+            'farmer': 0,
+            'shepherd': 0,
+            'hunter': 0,
+            'miner': 0,
+            'woodcutter': 0,
+            'artisan': 0,
+            'homekeeper': 0,
+            'government': 0,
+            'retail': 1
+        }
     }
     for material_class in ref.material_class_dct.keys():
         town['resource'][material_class] = {}
         for material_type in ref.material_class_dct[material_class]:
             town['resource'][material_class][material_type] = {
-                'intrinsic value': 0,
                 'harvestable': 0,
                 'available': 0
             }
