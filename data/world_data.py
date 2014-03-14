a670a20db6229052289fa839e1ec0a11493f1fa4
gremdavel <MGD1981@gmail.com>
2014-03-13 20:50:15 -0400

    Need to fix extremely slow terrain generation algorithm.

--- data/world_data.py
+++ data/world_data.py
@@ -1,9 +1,9 @@
-from random import randint
+from random import randint, choice, shuffle
 import pdb
 
-def get_new_world(size=128):
+def get_new_world(size=16):
     world = {
-        'grid': [[0] * size] * size,
+        'grid': [list([0] * size) for x in xrange(size)],
         'size': size
     }
     world['grid'] = generate_terrain(world['grid'], size)
@@ -16,7 +16,6 @@ def generate_terrain(grid, size):
     tiles_remaining = 0
     for row in grid:
         tiles_remaining += len(row)
-
     g = None
     w = None
     r = None
@@ -27,29 +26,27 @@ def generate_terrain(grid, size):
     grid[g[0]][g[1]] = 'g'
     grid[w[0]][w[1]] = 'w'
     grid[r[0]][r[1]] = 'r'
-   pdb.set_trace()
-     tiles_remaining -= 3
+    tiles_remaining -= 3
     while tiles_remaining > 0:
-        print "tiles_remaining: %r" % tiles_remaining
-
         for t in ['g', 'w', 'r']:
-            print "Tile: %r" % t
-            raw_input()
-            for next_t in [(eval(t)[0] - 1, eval(t)[1]),
-                           (eval(t)[0] + 1, eval(t)[1]),
-                           (eval(t)[0], eval(t)[1] - 1),
-                           (eval(t)[0], eval(t)[1] + 1)]:
-                print next_t
-
-                try:
-                    if grid[next_t[0]][next_t[1]] == 0:
-                        grid[next_t[0]][next_t[1]] = t
-                        exec(t + " = next_t")
-                        tiles_remaining -= 1
-                        print "%r = %r then break" % (t, next_t)
-                   
-                        next                        
-                except IndexError:
-                    print "Index Error, continue"
-                    
+            adjacents = [(eval(t)[0] - 1, eval(t)[1]),
+                         (eval(t)[0] + 1, eval(t)[1]),
+                         (eval(t)[0], eval(t)[1] - 1),
+                         (eval(t)[0], eval(t)[1] + 1)]
+            shuffle(adjacents)
+            for next_t in adjacents:
+                x = next_t[0]
+                y = next_t[1]
+                if x < 0 or x >= size or y < 0 or y >= size:
+                    continue    
+                if grid[next_t[0]][next_t[1]] == 0:
+                    grid[next_t[0]][next_t[1]] = t
+                    exec(t + " = next_t")
+                    tiles_remaining -= 1
+                else:
                     continue
+            jump = (randint(0, size-1), randint(0, size-1))
+            jumped_to = grid[jump[0]][jump[1]]
+            if jumped_to != 0:
+                exec(jumped_to + " = " + str(jump))
+    return grid
