2116cdd125f6391c8d4ad2e3eb44f4704107ff29
Matthew Davis <mgd1981@gmail.com>
2014-03-17 14:12:46 +0000

    site_data.py edited online with Bitbucket

--- data/site_data.py
+++ data/site_data.py
@@ -1,6 +1,6 @@
 import entities
 import reference_data as ref
-from random import randint
+from random import randint, choice
 
 
 class Site()
@@ -24,13 +24,16 @@ class Site()
             while terrain_type not in [0, 't']:
                 x = randint(0, size-1), y = randint(0, size-1)
             self.location = [x,y]
-            self.structure = (Structure().generate('terrain_type'))
-
             if site_type = 'random':
                 if randint(1,3) == 1:
                     self.site_type = 'adventure'
                 else:
                     self.site_type = 'resource'
+            else:
+                self.site_type = site_type
+                
+            self.structure = Structure().generate(ref.terrain_dct[terrain_type], self.site_type)
+            
         self.set_site_id()
         return self
 
@@ -52,5 +55,13 @@ class Structure():
     def __init__(self):
         self.structure_type = None
         
-    def generate(self, terrain_type):
-        pass
+    def generate(self, terrain_type='random', site_type='random'):
+
+        if terrain_type == 'random' and site_type == 'random':
+            self.structure_type = choice(ref.structure_type_dct.keys())
+        elif site_type == 'random':
+            self.structure_type = choice(ref.structure_class_dct[terrain_type])
+        elif terrain_type == 'random':
+            self.structure_type = choice(ref.site_class_dct[site_type])
+
+        return self
