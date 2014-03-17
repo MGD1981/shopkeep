6388a1c7a0f76ee1f1d972027c3b41c9ea85a56f
Matthew Davis <mgd1981@gmail.com>
2014-03-17 17:21:19 +0000

    entities.py edited online with Bitbucket

--- data/entities.py
+++ data/entities.py
@@ -4,6 +4,7 @@ player = {}
 weapons = {}
 components = {}
 heroes = {}
+monsters = {}
 sites = {}
 world = {}
 town = {}
@@ -11,7 +12,7 @@ shop = {}
 
 
 def initialize():
-    global player, weapons, components, heroes, sites, world, town
+    global player, weapons, components, heroes, monsters, sites, world, town
     import world_data
     world = world_data.get_new_world(32)
     import town_data
@@ -30,6 +31,10 @@ def initialize():
         'next id': 1,
         'object list': []
     }
+    monsters = {
+        'next id': 1,
+        'object list': []
+    }
     sites = {
         'next id': 1,
         'object list': []
@@ -39,17 +44,18 @@ def initialize():
 
 
 def load(save_name):
-    global player, weapons, components, heroes, sites, world, town, shop
+    global player, weapons, components, heroes, monsters, sites, world, town, shop
     save_file = open('saves/%s' % save_name)
     dict_list = pickle.load(save_file)
     player = dict_list[0]
     weapons = dict_list[1]
     components = dict_list[2]
     heroes = dict_list[3]
-    sites = dict_list[4]
-    world = dict_list[5]
-    town = dict_list[6]
-    shop = dict_list[7]
+    monsters = dict_list[4]
+    sites = dict_list[5]
+    world = dict_list[6]
+    town = dict_list[7]
+    shop = dict_list[8]
     save_file.close()
     return
 
@@ -60,7 +66,8 @@ def save(save_name):
     dict_list = [player,
                  weapons,
                  components,
-                 heroes, 
+                 heroes,
+                 monsters,
                  sites, 
                  world,
                  town,
