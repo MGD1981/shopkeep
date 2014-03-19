409f28e08b870096386fc1406fde8ca4b8da7436
Matthew Davis <mgd1981@gmail.com>
2014-03-19 20:02:16 +0000

    player_data.py edited online with Bitbucket

--- data/player_data.py
+++ data/player_data.py
@@ -4,7 +4,11 @@ import entities
 def get_new_player():
     player = {
         'location': [0, 0],
-        'coins': 0,
+        'coins': {
+            'copper': 0,
+            'silver': 0,
+            'gold': 0
+        },
         'appraisal skill': {
             'wood': 0,
             'fiber': 0,
