a8513b96ad2edb07e9c393bb4d647e902e4c5ccc
Matthew Davis <mgd1981@gmail.com>
2014-03-24 17:53:25 +0000

    shop_data.py edited online with Bitbucket

--- data/shop_data.py
+++ data/shop_data.py
@@ -5,5 +5,15 @@ from random import choice
 class Shop():
 	"""Shop class object"""
 	
-	def __init__():
-		pass
+	def __init__(self):
+		self.player_owned = False
+		self.shop_grid = None
+		self.security = None
+		self.decor = None
+		
+	def generate(self, arg):
+		"""Generates a new shop object."""
+		if arg == 'player':
+			self.shop_grid = []
+			
+		return self
