15d753e0c7315ecb081d604135c638054db527ec
MGD1981 <MGD1981@gmail.com>
2014-05-29 00:52:38 -0400

    Game collision detection now uses native PyGame sprite collision detection

--- display/sprites.py
+++ display/sprites.py
@@ -2,6 +2,7 @@ import os, sys
 import pygame as pg
 from pygame.locals import *
 from data import entities, reference_data as ref
+import copy
 
 def load_image(name, colorkey=None):
     try:
@@ -34,19 +35,19 @@ class Person(pg.sprite.Sprite):
 
     def update(self, game):
         if game.keys[K_LEFT]:
-            if self.validate(0,-1):
+            if self.validate(game, 0,-1):
                 self.move(0,-1)
                 self.refresh(game)
         if game.keys[K_RIGHT]:
-            if self.validate(0,1):
+            if self.validate(game, 0,1):
                 self.move(0,1)
                 self.refresh(game)
         if game.keys[K_UP]:
-            if self.validate(1,-1):
+            if self.validate(game, 1,-1):
                 self.move(1,-1)
                 self.refresh(game)
         if game.keys[K_DOWN]:
-            if self.validate(1,1):
+            if self.validate(game, 1,1):
                 self.move(1,1)
                 self.refresh(game)
 
@@ -62,36 +63,15 @@ class Person(pg.sprite.Sprite):
             self.index = 0
         self.image = self.images[self.index]
         
-    def validate(self, axis, direction):
-        #axis: 0=x, 1=y
-        #direction: 1=right, down  -1=left, up
+    def validate(self, game, axis, direction):
         vector = ref.player_speed * direction
-
-        #TODO: Use native pygame sprite collision detection
-        #      by creating two kinds of sprite groups:
-        #      one passable and one not passable
-
-        try:
-            for sprite_border in [
-                (0,0),
-                (1,0),
-                (1,1),
-                (0,1)
-                                 ]:
-                if not ref.shop_tile_dct[
-                    entities.shop['object'].shop_grid[
-                        ((entities.player['object'].location[1] + 
-                        (sprite_border[1] * (self.rect.height-1))) + 
-                        axis*vector)/ref.tile_size][
-                        ((entities.player['object'].location[0] + 
-                        (sprite_border[0] * (self.rect.width-1))) + 
-                        (-axis+1)*vector)/ref.tile_size]]['passable'
-                ]:
-                    return False
-            return True
-        except IndexError:
-            return False 
-
+        test_sprite = pg.sprite.Sprite()
+        test_sprite.rect = copy.deepcopy(self.rect)
+        test_sprite.rect[0] = test_sprite.rect[0] + (-axis+1)*vector
+        test_sprite.rect[1] = test_sprite.rect[1] + axis*vector
+        if len(pg.sprite.spritecollide(test_sprite, game.screens['world'].impassable_shop_tiles, False)) > 0:
+            return False
+        return True
 
     def move(self, axis, direction):
         vector = ref.player_speed * direction
